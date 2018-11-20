b84e519b1ac15aae86f87a8fbed83313eb04e922
Pushpal <pushpalroy2007@gmail.com>
2018-11-21 02:06:58 +0530

    Minor changes.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ImageLoaderHelper.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ImageLoaderHelper.java
@@ -8,9 +8,9 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
-public class ImageLoaderHelper {
+class ImageLoaderHelper {
     private static ImageLoaderHelper sInstance;
-    private final LruCache<String, Bitmap> mImageCache = new LruCache<String, Bitmap>(20);
+    private final LruCache<String, Bitmap> mImageCache = new LruCache<>(20);
     private ImageLoader mImageLoader;
 
     private ImageLoaderHelper(Context applicationContext) {
@@ -29,7 +29,7 @@ public class ImageLoaderHelper {
         mImageLoader = new ImageLoader(queue, imageCache);
     }
 
-    public static ImageLoaderHelper getInstance(Context context) {
+    static ImageLoaderHelper getInstance(Context context) {
         if (sInstance == null) {
             sInstance = new ImageLoaderHelper(context.getApplicationContext());
         }
@@ -37,7 +37,7 @@ public class ImageLoaderHelper {
         return sInstance;
     }
 
-    public ImageLoader getImageLoader() {
+    ImageLoader getImageLoader() {
         return mImageLoader;
     }
 }
